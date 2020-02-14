02d9701dad86a5f15c77b2bd80c4aea794152ae1
Karunio <sa_mang@naver.com>
2020-02-14 14:25:47 +0900

    exam01

--- exam01/main.c
+++ exam01/main.c
@@ -1,10 +1,10 @@
 /*
   filename - main.c
   version - 1.0
-  description - 기본 메인 함수
+  description - 기본 메인 함수, exam01
   --------------------------------------------------------------------------------
-  first created - 2020.02.01
-  writer - Hugo MG Sung.
+  first created - 2020.02.14
+  writer - Karunio
 */
 
 #include <stdio.h>
